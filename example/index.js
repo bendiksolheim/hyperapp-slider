4f1d1c41606d84e8adebc25be773ab1a6ebb95f4
Bendik Solheim <bendik@solheim.im>
2018-04-01 23:12:33 +0200

    Example with two sliders in same app

--- example/index.js
+++ example/index.js
@@ -3,16 +3,20 @@ import { Slider } from '../src';
 import devtools from 'hyperapp-redux-devtools';
 
 const state = {
-  slider: Slider.state({ min: 0, max: 100, value: 100 })
+  slider: Slider.state({ min: 20, max: 60, value: 50 }),
+  sliderTwo: Slider.state({ min: 120, max: 122, value: 121 })
 };
 const actions = {
-  slider: Slider.actions
+  slider: Slider.actions,
+  sliderTwo: Slider.actions
 };
 
 const view = (state, actions) => (
   <div>
     <div>Value: {state.slider.value}</div>
     <Slider state={state.slider} actions={actions.slider} />
+    <div>Value: {state.sliderTwo.value}</div>
+    <Slider state={state.sliderTwo} actions={actions.sliderTwo} />
   </div>
 );
 
